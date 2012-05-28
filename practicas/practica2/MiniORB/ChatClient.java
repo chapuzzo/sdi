6b5d5e6be4c176e854f1f0992fa6c9291784c1ba
Luis M Mahiques Carrasco <luismax@gmail.com>
2012-05-28 12:16:34 +0200

    preparando para bufferedInput

--- practicas/practica2/MiniORB/ChatClient.java
+++ practicas/practica2/MiniORB/ChatClient.java
@@ -1,107 +1,121 @@
 import java.io.*;
 
-
 public class ChatClient {
 
-	
-	 	String hostc, hostNS;
-	    int portc, portNS, oid = 0, iid = 0;
-	    MiniORB orb = null;
-	    NameService NS = null;
+	String hostc, hostNS;
+	int portc, portNS, oid = 0, iid = 0;
+	MiniORB orb = null;
+	NameService NS = null;
 
-	    public static void main (String args[]) {
-	        ChatClient c = new ChatClient();
+	public static void main(String args[]) {
+		ChatClient c = new ChatClient();
 
-	        if (! c.parseArgs(args)){
-	            System.err.println(
-	                "uso:\n\t $ java ChatClient <host> <port> <hostNS> <portNS>"
-	                );
-	        }
-	        else
-	            c.pruebaChatClient();
-	    }
+		if (!c.parseArgs(args)) {
+			System.err
+					.println("uso:\n\t $ java ChatClient <host> <port> <hostNS> <portNS>");
+		} else
+			c.pruebaChatClient();
+	}
 
+	public void pruebaChatClient() {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		PrintStream out = new PrintStream(System.out);
 
-	    public void pruebaChatClient(){
-	    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	    	PrintStream out = new PrintStream(System.out);
-	    	
-	        orb = new MiniORB(hostc,portc,hostNS,portNS);
-	        orb.serve();
+		orb = new MiniORB(hostc, portc, hostNS, portNS);
+		orb.serve();
 
-	        NS = orb.getNameService();
-	        try{
-	        	out.println("Escribe un nombre de usuario: ");
-	        	String user = in.readLine();
-	        	ChatUser pCU,CU = new ChatUserClass(user);
-	        	ChatServer CS = (ChatServer)NS.resolve("cs");
-	        	if (CS.getUser(CU.getName()) == null)//{
-	        		CS.registerUser(CU.getName(), CU);
-	        		//pCU = (ChatUser)CS.getUser(CU.getName());
-	        	//}
-	        	//else
-	        	//{
-	        		//pCU = (ChatUser)CS.getUser(CU.getName());
-	        	//}
-	        	ChatChannel CC = CS.getChannel("cc");
-	        	CC.joinUser(CU);
-	        	String message = "";
-	        	while (!message.equals("fin")){
-	        		message = in.readLine();
-		        	CC.sendMessage(new ChatMessageClass(CU.getName() +"> "+ message));	        		
-	        	}
-	        	CC.leaveUser(CU);
-	        	System.exit(0);
-	        }
-	        catch(Exception E){
-	            E.printStackTrace();
-	        }
+		NS = orb.getNameService();
+		try {
+			out.println("Escribe un nombre de usuario: ");
+			String user = in.readLine();
+			ChatUser CU = new ChatUserClass(user);
+			ChatServer CS = (ChatServer) NS.resolve("cs");
+			if (CS.getUser(CU.getName()) == null)
+				CS.registerUser(CU.getName(), CU);
+			ChatChannel CC = CS.getChannel("cc");
+			CC.joinUser(CU);
+			String message = "";
+			while (!message.equals("fin")) {
+				message = in.readLine();
+				CC.sendMessage(new ChatMessageClass(CU.getName() + "> "
+						+ message));
+			}
+			CC.leaveUser(CU);
+			System.exit(0);
+		} catch (Exception E) {
+			E.printStackTrace();
+		}
 
+	}
 
-	    }
+	public boolean parseArgs(String args[]) {
 
-	    public boolean parseArgs(String args[]){
+		if (args.length < 4) {
+			System.err.println("Invalid argument number: " + args.length);
+			return false;
+		}
 
-	        if (args.length < 4){
-	            System.err.println("Invalid argument number: " + args.length);
-	            return false;
-	        }
+		hostc = args[0];
 
-	        hostc = args[0];
+		try {
+			portc = (new Integer(args[1])).intValue();
+		} catch (Exception e) {
+			System.err.println("Invalid port number: " + args[1]);
+			return false;
+		}
 
-	        try {
-	            portc = (new Integer(args[1])).intValue();
-	        } catch (Exception e) {
-	            System.err.println("Invalid port number: " + args[1]);
-	            return false;
-	        }
+		hostNS = args[2];
 
-	        hostNS = args[2];
+		try {
+			portNS = (new Integer(args[3])).intValue();
+		} catch (Exception e) {
+			System.err.println("Invalid port number: " + args[3]);
+			return false;
+		}
 
-	        try {
-	            portNS = (new Integer(args[3])).intValue();
-	        } catch (Exception e) {
-	            System.err.println("Invalid port number: " + args[3]);
-	            return false;
-	        }
+		if (args.length > 4)
+			try {
+				oid = (new Integer(args[4])).intValue();
+			} catch (Exception e) {
+				System.err.println("Invalid oid number: " + args[4]);
+				return false;
+			}
 
-	        if (args.length > 4)
-	        try {
-	            oid = (new Integer(args[4])).intValue();
-	        } catch (Exception e) {
-	            System.err.println("Invalid oid number: " + args[4]);
-	            return false;
-	        }
+		if (args.length > 5)
+			try {
+				iid = (new Integer(args[5])).intValue();
+			} catch (Exception e) {
+				System.err.println("Invalid iid number: " + args[5]);
+				return false;
+			}
 
-	        if (args.length > 5)
-	        try {
-	            iid = (new Integer(args[5])).intValue();
-	        } catch (Exception e) {
-	            System.err.println("Invalid iid number: " + args[5]);
-	            return false;
-	        }
+		return true;
 
-	        return true;
-
-	    }
+	}
 }
+
+
+
+/*private static void setTerminalToCBreak() throws IOException, InterruptedException {
+
+        ttyConfig = stty("-g");
+
+        // set the console to be character-buffered instead of line-buffered
+        stty("-icanon min 1");
+
+        // disable character echoing
+        stty("-echo");
+    }
+
+   
+    private static String stty(final String args)
+                    throws IOException, InterruptedException {
+        String cmd = "stty " + args + " < /dev/tty";
+
+        return exec(new String[] {
+                    "sh",
+                    "-c",
+                    cmd
+                });
+    }
+*/
--- practicas/practica2/MiniORB/ChatUserClass.java
+++ practicas/practica2/MiniORB/ChatUserClass.java
@@ -1,21 +1,21 @@
+public class ChatUserClass implements ChatUser {
+	String name;
+	String prompt = "Escribe tu mensaje -> ";
 
-public class ChatUserClass implements ChatUser{
-    String name;
-    String prompt = "Escribe tu mensaje -> ";
+	public ChatUserClass(String name) {
+		this.name = name;
+		System.out.print(prompt);
+	}
 
-    public ChatUserClass(String name){
-        this.name = name;
-        System.out.print(prompt);
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public String getName(){
-        return this.name;
-    }
-
-    public void sendMessage(ChatMessage m){
-    	int i = 80;
-    	while (i-- > 0) System.out.print("\b \b");
-        System.out.println("\r" + m.getText());
-        System.out.print(prompt);
-    }
+	public void sendMessage(ChatMessage m) {
+		int i = 80;
+		while (i-- > 0)
+			System.out.print("\b \b");
+		System.out.println("\r" + m.getText());
+		System.out.print(prompt);
+	}
 }
